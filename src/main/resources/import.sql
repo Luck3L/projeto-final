b5ab63b1594ab8188f7075a53e3c311db0790c01
Luck3L <98151219+Luck3L@users.noreply.github.com>
2026-04-20 23:58:26 -0300

    Add SQL insert statements for users and courses

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+--INSERT INTO courses (name) VALUES ('Novo Curso');
+INSERT INTO usuarios (email, nome, role, senha) VALUES ('admin','admin','ADMIN','$2a$12$njFULkQi9nK5TgVQKiMO7ubMIPa2TvCSbNgtpUzQcqxwk8y/hdVS.');
