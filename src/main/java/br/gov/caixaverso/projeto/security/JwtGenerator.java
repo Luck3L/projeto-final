169d5cb8c89bb01f2498c943e50ac4733222ccd9
Luck3L <98151219+Luck3L@users.noreply.github.com>
2026-04-21 00:54:18 -0300

    Add issuer configuration to JwtGenerator

--- src/main/java/br/gov/caixaverso/projeto/security/JwtGenerator.java
+++ src/main/java/br/gov/caixaverso/projeto/security/JwtGenerator.java
@@ -3,14 +3,19 @@ package br.gov.caixaverso.projeto.security;
 import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.jwt.Claims;
 
 @ApplicationScoped
 public class JwtGenerator {
 
+    @ConfigProperty(name = "mp.jwt.verify.issuer")
+    String issuer;
+
     public String generateSignJwt(String nome, String role) {
         return Jwt.claim(Claims.sub, nome)
                 .groups(role)
+                .issuer(issuer)
                 .jws()
                 .sign();
     }
